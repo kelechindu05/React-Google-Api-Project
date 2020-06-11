29c24bc04a5a8cf3ccb909959fcc090b43789f89
Kelechi Ndu <kndu@iqs-solutions.com>
2020-06-11 03:45:02 +0100

    made changes to search input box

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function Locate ({panTo}){
   ><img src="/compass.svg" alt="compass- locate me"/></button>
 }
 
-function Search(){
+function Search({panTo}){
   const {
     ready, 
     value, 
@@ -149,36 +149,44 @@ function Search(){
       radius: 20 * 1000,
     },
   });
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    //clearSuggestions();
+
+    try {
+      const results = await 
+      ({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (error) {
+      console.log(" Error: ", error);
+    }
+  };
+
   return (
     <div className="search">
-      <Combobox onSelect={async (address) =>{
-        try{
-          const results = await getGeocode({address});
-          console.log(results[0])
-        }catch(error){
-          console.log(address);
-        }
-
-        
-      }}
-      >
-
-          <ComboboxInput value ={value} onChange={(e) =>{
-            setValue(e.target.value);
-          }}
+    <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
           disabled={!ready}
-          placeholder = "Enter a Location"
-          />
+          placeholder="Search your location"
+        />
         <ComboboxPopover>
-           <ComboboxList>
+          <ComboboxList>
             {status === "OK" &&
               data.map(({ id, description }) => (
                 <ComboboxOption key={id} value={description} />
               ))}
           </ComboboxList>
         </ComboboxPopover>
-    </Combobox>
-   </div>
+      </Combobox>
+    </div>
   )
 
 }
